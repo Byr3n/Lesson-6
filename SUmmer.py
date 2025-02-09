0af954b5f91715f14556ee9d88beeaa0100d8fda
Byr3n <sealunion45@gmail.com>
2025-02-08 18:23:49 -0800

    first publish

---
+++ SUmmer.py
@@ -0,0 +1,12 @@
+temp=(float(input("Enter the today's temperature")))
+if temp<10:
+    print("Wear heavy clothes")
+elif temp<15:
+    print("Sweaters and pants")
+elif temp<20:
+    print("wear a long sleeved-shirt")
+elif temp<25:
+    print("Wear some light clothing")
+else:
+    print("Wear summer like clothing and consider sunscreen")
+    
